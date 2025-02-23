e99d3856e89b02c34a076943a9162316885e65b4
135790-diya <diyathresia@gmail.com>
2025-02-23 17:04:24 +0530

    product-app

---
+++ src/components/ShoppingCartPage.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+
+const ShoppingCartPage = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/81Zr43yLXvL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Arkasto Decorative Silent Wall Clock</h5>
+                                        <p class="card-text">Dual-Purpose Design.</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/71kD-NwgjAL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Amazon Brand - Umi Tempered Glass Wall Art Painting Photo Frame</h5>
+                                        <p class="card-text">CLOCK WITH CRYSTAL EMBEDDED PICTURE FRAME.</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/61YrS69FB9L._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/713o-qBgADL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/810aHSfqRkL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/51LXuHjy9lL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/910CyLEHPiL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/81lzYV460dL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/61ks0EQTR9L._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <img src="https://m.media-amazon.com/images/I/81pmTbLGpfL._AC_UY327_FMwebp_QL65_.jpg" class="card-img-top" alt=""/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clock</h5>
+                                        <p class="card-text"> Handmade Geometric Design with Large and Easy to Read Numerals..</p>
+                                        <a href="#" class="btn btn-primary">More info.</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShoppingCartPage
